b307e07cdb130c0d293c9c527346822bd28d2a90
Andreas Menzel <mail@andreas-menzel.com>
2026-02-08 17:58:01 +0100

    build: fix npm run dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import { version } from './package.json'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
     vue(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      'shared': path.resolve(__dirname, '../shared/index.ts')
+    }
+  },
   server: {
     proxy: {
         '/api': 'http://localhost:3000'
