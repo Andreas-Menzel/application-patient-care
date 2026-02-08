674a12b94350607aeaf50b718c75205f252e1244
Andreas Menzel <mail@andreas-menzel.com>
2026-02-08 08:53:08 +0100

    fix: fix baseUrl

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,5 +2,5 @@ import { initQueryClient } from '@ts-rest/vue-query';
 import { apiContract } from 'shared';
 
 export const api = initQueryClient(apiContract, {
-  baseUrl: 'http://localhost:3000', // TODO: update to use production url
+  baseUrl: '',
 });
